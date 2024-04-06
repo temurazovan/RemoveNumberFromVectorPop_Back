6b5d72e56b5e49c17f2fc3b3091a5bc3d770b119
temurazovan <nadezhdadieslast@gmail.com>
2024-04-06 17:55:16 +0400

    first commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <vector>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::vector<int> vector;
+    int size;
+    int numbers;
+    int numberToDelete;
+
+    std::cout << "Input vector size: ";
+    std::cin >> size;
+    vector.resize(size);
+
+    std::cout << "Input numbers: ";
+    for (int i = 0; i < size; i++){
+        std::cin >> numbers;
+        vector[i] = numbers;
+    }
+
+
+
 }
